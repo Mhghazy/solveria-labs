e6c6138bbaeceec8bd2dc34ffd69fc3d5c711291
Mhghazy <mahmoudhghazy@hotmail.com>
2026-04-13 21:20:52 +0200

    Configure GitHub Pages Actions Deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/solveria-labs',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;

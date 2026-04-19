c344b77a6f96158d971b86c10ede010ceaedf268
Mohamed Hossam <https://github.com/mohamedhossamkaml>
2026-04-20 00:10:50 +0200

    fix: sync Tailwind v4 setup and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: 'export',
+//   basePath: '/solveria-labs',
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/solveria-labs',
-  images: {
-    unoptimized: true,
+  turbopack: {
+    root: __dirname,
   },
 };
 
